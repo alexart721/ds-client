95968410851e977d0021a853919a152940677f89
Alex Teodorescu <ateodorescu721@gmail.com>
2021-07-06 11:06:03 -0400

    refactor sidebar

--- components/SideBar/SideBar.tsx
+++ components/SideBar/SideBar.tsx
@@ -1,14 +1,29 @@
 import ChannelsList from '../ChannelList/ChannelsList';
+import MyIssuesList from '../MyIssuesList/MyIssuesList';
 import { useSelector } from '../../lib/hooks/useTypedSelector';
-import { shallowEqual } from 'react-redux';
+import { Menu } from 'antd';
+
+const { SubMenu } = Menu;
 
 const SideBar = () => {
   const channels = useSelector((state) => state.channels);
+  const issues = useSelector((state) => state.issues);
   console.log(channels);
 
   return (
-    <div>
-      <ChannelsList channels={channels} />
+    <div style={{ width: "14vw", height: "100vh", background: "#001529" } }>
+      <Menu
+        defaultOpenKeys={['sub1','sub2']}
+        mode="inline"
+        theme="dark"
+      >
+        <SubMenu key="sub1" title="Channels" >
+          <ChannelsList channels={channels} />
+        </SubMenu>
+        <SubMenu key="sub2" title="My Issues" >
+          <MyIssuesList issues={issues} />
+        </SubMenu>
+      </Menu>
     </div>
   );
 };
