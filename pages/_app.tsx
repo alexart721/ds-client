2967d0736178e60b45f66ab995d38a9ace5ad269
Ryan Baker <rbaker699@gmail.com>
2021-07-06 17:56:55 -0700

    Add socket io backbone

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,8 +6,10 @@ import { store } from '../lib/redux/store';
 import { myChannelsSlice, myIssuesSlice } from '../lib/redux/reducers';
 import 'antd/dist/antd.css';
 import { getUserApi, checkToken } from '../services';
+import sockets from '../sockets';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  sockets.init();
   const refreshGuard = async (accessToken: string) => {
     const response = await checkToken(accessToken, 'User').then(res => res.json());
     const user = await getUserApi(accessToken, response.id).then(res => res.json());
--- services/index.ts
+++ services/index.ts
@@ -1,7 +1,9 @@
 // export const BASE_URL = 'http://localhost/api';
 // export const BASE_AUTH_URL = 'http://localhost/auth';
-export const BASE_URL = 'http://localhost:4000';
-export const BASE_AUTH_URL = 'http://localhost:4002';
+// export const BASE_URL = 'http://localhost:4000';
+// export const BASE_AUTH_URL = 'http://localhost:4002';
+export const BASE_URL = 'http://172.31.34.163/api';
+export const BASE_AUTH_URL = 'http://172.31.34.163/auth';
 
 export * from './channelsApi';
 export * from './issuesApi';
