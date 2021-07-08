7d47b225765461432241d34db34a6a3c5ee6b6a9
Dhruvang1996 <dhruvang.merchant@ucalgary.ca>
2021-07-07 22:12:26 -0600

    eslint is working

--- components/MessageItem/MessageItem.tsx
+++ components/MessageItem/MessageItem.tsx
@@ -1,22 +1,11 @@
 import React, { FC } from 'react';
 import { MessageData } from '../../lib/types';
 
-<<<<<<< HEAD
-const MessageItem: FC<{message: MessageData}> = ({ message }) => {
-  return (
-    <div>
-      <div>{message.messageOwnerName}</div>
-      <div>{message.content}</div>
-    </div>
-  )
-}
-=======
 const MessageItem: FC<{ message: MessageData }> = ({ message }) => (
   <>
     <div>{message.messageOwnerName}</div>
     <div>{message.content}</div>
   </>
 );
->>>>>>> main
 
 export default MessageItem;
